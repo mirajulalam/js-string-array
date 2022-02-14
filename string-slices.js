0afb51e5ddd40e733272b293ad7049ae6c3219dc
mirajul alam <alifmirajulalam@gmail.comm>
2022-02-14 12:48:55 +0600

    second added

--- string-slices.js
+++ string-slices.js
@@ -1,3 +1,27 @@
 const anthem = 'amar sonar bangla ami tomai valobashi';
-const words = anthem.split('');
-console.log(words);
+const words = anthem.split(' ');
+const withoutA = anthem.split('a');
+// console.log(withoutA);
+
+// slice
+const smallSlice = anthem.slice(11, 17);
+// console.log(smallSlice);
+
+// substring
+const anotherPart = anthem.substring(18, 21);
+// console.log(anotherPart);
+
+// concut
+const first = 'Amader';
+const second = 'City';
+// const fullName = first + second;
+const fullString = first.concat(second).concat('Dhaka').concat('Bangladesh');
+// console.log(fullString);
+
+const words2 = ['a', 'b', 'c', 'd'];
+// const allJoin = words2.join('');
+// const allJoin = words2.join(' ');
+// const allJoin = words2.join(',');
+// const allJoin = words2.join(', ');
+const allJoin = words2.join('www');
+console.log(allJoin);
